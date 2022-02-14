c4d333ade65aa4ba113fdc3517b3330a66520693
Dennis Patzer <d.patzer@heimrich-hannot.de>
2022-02-14 17:27:47 +0100

    see cl 0.5.5

--- src/FrontendModule/NavigationTrait.php
+++ src/FrontendModule/NavigationTrait.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2021 Heimrich & Hannot GmbH
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
@@ -50,7 +50,10 @@ trait NavigationTrait
         /** @var FrontendTemplate|object $objTemplate */
         $objTemplate = new \FrontendTemplate($this->navigationTpl);
 
-        $objTemplate->setData($module->arrData);
+        if (isset($module) && \is_array($module)) {
+            $objTemplate->setData($module->arrData);
+        }
+
         $objTemplate->pid = $pid;
         $objTemplate->type = static::class;
         $objTemplate->cssID = $this->cssID; // see #4897
